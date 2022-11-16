0917662e6587244254cd97c496a53b8aeb1b4e4b
Cho Changhun <cvcvcx@naver.com>
2022-11-16 22:44:48 +0900

    [feat]: <cart>Package추가 <order> (OrderVO,OrderItemVO) 추가

---
+++ src/main/java/com/fakenoonting/www/order/vo/OrderItemVO.java
@@ -0,0 +1,24 @@
+package com.fakenoonting.www.order.vo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class OrderItemVO {
+
+    //자동으로 생성되는 이 테이블의 생성 키
+    private Long id; //(name = "order_item_id",pk)
+    //Order에 속하는 (Order)1 대 다(OrderItem) 관계
+    private Long orderId; //(name = "order_id",fk)
+    //상품과는 1:1관계가 된다. (OrderItem) 1 : 1 (Product)
+    private Long productId;//(name = "product_id,fk)
+
+    public OrderItemVO(Long id, Long orderId, Long productId) {
+        this.id = id;
+        this.orderId = orderId;
+        this.productId = productId;
+    }
+}
