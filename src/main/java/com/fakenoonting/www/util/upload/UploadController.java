4d7e7e3fdd6b74ac6dcda4be367c3cb5fbc76d95
Cho Changhun <cvcvcx@naver.com>
2022-11-28 16:39:43 +0900

    [update]: 상세 페이지에 있던 bootstrap 스크립트  header로 이동 및 Util 패키지 생성 후 이미지 업로드시 서버의 이미지 저장 폴더에 이미지 폴더 생성기능 추가

---
+++ src/main/java/com/fakenoonting/www/util/upload/UploadController.java
@@ -0,0 +1,53 @@
+package com.fakenoonting.www.util.upload;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+@Slf4j
+@RequestMapping("util/upload")
+public class UploadController {
+
+    //servlet context에 설정되어있음(빈 객체 등록)
+    @Resource(name = "uploadPath")
+    String uploadPath;
+    //Post 요청이 왔을때 여러 파일이여도 모두 처리할 수 있게끔 배열처리
+    //현재 업로드 요청이 있었을 때를 폴더로 삼아서 저장하도록 한다.
+
+    //만약, 프로필 같은 사진을 업로드 한다고 해도, 여기로 요청만 보내진다면 서버로 저장할 수 있음
+    //상품 후기 글도 마찬가지
+    @PostMapping("/uploadAjax")
+    public void uploadAjax(MultipartFile[] uploadFile){
+
+        log.info("uploadAjaxPost요청 진행중...");
+
+        String uploadFolder = uploadPath;
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        String str = sdf.format(date);
+        String datePath = str.replace("-", File.separator);
+
+        File savePath = new File(uploadFolder, datePath);
+
+        if(savePath.exists() == false){
+            savePath.mkdirs();
+        }
+
+        for(MultipartFile multipartFile: uploadFile){
+
+        }
+
+
+    }
+
+
+}
