82d27ebf7f264409c2233c7670318903fd528359
0rctic_fox <0rctic_fox@naver.com>
2022-12-08 21:53:51 +0900

    [Update] Product 패키지 코드 및 주석 정리

--- src/main/java/com/fakenoonting/www/product/dao/ProductDAO.java
+++ src/main/java/com/fakenoonting/www/product/dao/ProductDAO.java
@@ -12,7 +12,7 @@ public interface ProductDAO {
     // 썸네일 이미지 등록
     public int imageEnroll(ImgItemVO img);
     // 상세 본문 이미지 등록
-    int contentImageEnroll(ImgItemVO img);
+    public int contentImageEnroll(ImgItemVO img);
     // 제품 사이즈 등록
     public int sizeEnroll(ProductSizeVO size);
 
@@ -23,16 +23,16 @@ public interface ProductDAO {
     // productId 기준으로 해당 productImgItem 값들 추출
     public List<ImgItemVO> findImagesByProductId(ProductVO productVO);
     // productId 기준으로 해당 productContentImgItem 값들 추출
-    List<ImgItemVO> findContentImgByProductId(ProductVO productVO);
+    public List<ImgItemVO> findContentImgByProductId(ProductVO productVO);
     // productId 기준으로 해당 productSize 값들 추출
-    List<ProductSizeVO> findSizeByProductId(ProductVO productVO);
+    public List<ProductSizeVO> findSizeByProductId(ProductVO productVO);
     
     // productId 기준으로 해당 productVO를 삭제
     public int deleteProduct(ProductVO productId);
     // productId 기준으로 해당 product 썸네일 이미지를 삭제
     public int deleteImg(ProductVO productId);
     // productId 기준으로 해당 product 상세 본문 이미지를 삭제
-    int deleteContentImg(ProductVO productId);
+    public int deleteContentImg(ProductVO productId);
     // productId 기준으로 해당 product 사이즈 삭제
     public int deleteSize(ProductVO productId);
 
